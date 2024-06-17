5a57f0b875b2aa0e145c7521fa1365f1b1f34af3
nicu11 <niklas.cullberg@gmail.com>
2024-06-17 15:58:31 +0200

    Created basic Spring Boot project for the API and added connection to MySQL basics in application.properties

---
+++ MediaNoteMakerAPI/src/main/java/com/nicu11/MediaNoteMakerAPI/MediaNoteMakerApiApplication.java
@@ -0,0 +1,14 @@
+package com.nicu11.MediaNoteMakerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaNoteMakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaNoteMakerApiApplication.class, args);
+	}
+
+}
+
---
+++ MediaNoteMakerAPI/src/test/java/com/nicu11/MediaNoteMakerAPI/MediaNoteMakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicu11.MediaNoteMakerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaNoteMakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
